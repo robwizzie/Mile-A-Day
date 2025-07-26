5161ace3ec53c4cb99aad3c4cd6a6ed419043499
David Simmerman <davidsimmermancs@gmail.com>
2025-07-26 14:49:49 -0400

    Add check if apple_id already exists when creating new user

--- backend/src/controllers/users/createUser.ts
+++ backend/src/controllers/users/createUser.ts
@@ -19,6 +19,15 @@ export default async function createUser(req: Request, res: Response) {
 	const user_id = crypto.randomUUID().replaceAll('-', '');
 	const { username, apple_id, first_name = null, last_name = null } = req.body;
 
+	const existingAppleId = await db.query('SELECT user_id FROM users WHERE apple_id = $1', [apple_id]);
+
+	if (existingAppleId) {
+		return res.status(400).json({
+			error: 'Bad Request',
+			message: `User already exists with Apple ID ${apple_id}`
+		});
+	}
+
 	await db.query('INSERT INTO users (user_id, username, apple_id, first_name, last_name) VALUES ($1, $2, $3, $4, $5)', [
 		user_id,
 		username,
