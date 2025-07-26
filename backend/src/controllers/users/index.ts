6bea8cf2972f707563a735ca803e70c5cf6d75ec
David Simmerman <davidsimmermancs@gmail.com>
2025-07-26 15:27:44 -0400

    Add update user API

--- backend/src/controllers/users/index.ts
+++ backend/src/controllers/users/index.ts
@@ -2,3 +2,4 @@ export { default as getUser } from './getUser.js';
 export { default as createUser } from './createUser.js';
 export { default as searchUsers } from './searchUsers.js';
 export { default as deleteUser } from './deleteUser.js';
+export { default as updateUser } from './updateUser.js';
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, deleteUser, getUser, searchUsers } from '../controllers/users/index.js';
+import { createUser, deleteUser, getUser, searchUsers, updateUser } from '../controllers/users/index.js';
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/create', createUser);
 router.get('/search', searchUsers);
 router.get('/:id', getUser);
 router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
 
 export default router;
