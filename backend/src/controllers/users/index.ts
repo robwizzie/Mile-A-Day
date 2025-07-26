fa67affff2c5713bd1b037b7d14b87d5d4c45a37
David Simmerman <davidsimmermancs@gmail.com>
2025-07-26 15:06:25 -0400

    add delete user endpoint

--- backend/src/controllers/users/index.ts
+++ backend/src/controllers/users/index.ts
@@ -1,2 +1,4 @@
 export { default as getUser } from './getUser.js';
 export { default as createUser } from './createUser.js';
+export { default as searchUsers } from './searchUsers.js';
+export { default as deleteUser } from './deleteUser.js';
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { createUser, getUser } from '../controllers/users/index.js';
-import searchUsers from '../controllers/users/searchUsers.js';
+import { createUser, deleteUser, getUser, searchUsers } from '../controllers/users/index.js';
 
 const router = Router();
 
 router.post('/create', createUser);
 router.get('/search', searchUsers);
 router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
 
 export default router;
