7e7fa683f2524f7f51204637dea8bbd87f599b22
David Simmerman <davidsimmermancs@gmail.com>
2025-08-01 23:49:09 -0400

    rename apple_id to email

--- backend/src/controllers/users/searchUsers.ts
+++ backend/src/controllers/users/searchUsers.ts
@@ -4,15 +4,13 @@ import { PostgresService } from '../../services/DbService.js';
 const db = PostgresService.getInstance();
 
 export default async function searchUsers(req: Request, res: Response) {
-	const { username, apple_id } = req.query;
+	const { username, email } = req.query;
 
-	if (!username && !apple_id) {
-		return res.status(400).json({ error: 'Missing username or apple_id query parameter' });
+	if (!username && !email) {
+		return res.status(400).json({ error: 'Missing username or email query parameter' });
 	}
 
-	const results = await db.query(`SELECT * FROM users WHERE ${username ? 'username' : 'apple_id'} = $1`, [
-		username ?? apple_id
-	]);
+	const results = await db.query(`SELECT * FROM users WHERE ${username ? 'username' : 'email'} = $1`, [username ?? email]);
 
 	if (!results.length) {
 		return res.status(404).json({
