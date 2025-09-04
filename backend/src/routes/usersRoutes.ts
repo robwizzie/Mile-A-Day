402d5022a957d1608b876ab9889252d8ad349528
David Simmerman <davidsimmermancs@gmail.com>
2025-09-04 00:04:06 -0400

    remove create user

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { createUser, deleteUser, getUser, searchUsers, updateUser } from '../controllers/usersController.js';
+import { deleteUser, getUser, searchUsers, updateUser } from '../controllers/usersController.js';
 
 const router = Router();
 
-router.post('/create', createUser);
 router.get('/search', searchUsers);
 router.get('/:userId', getUser);
 router.delete('/:userId', deleteUser);
